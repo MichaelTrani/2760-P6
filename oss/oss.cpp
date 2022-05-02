35e04286feeecaeb2ee32a9d98c29efa5b6ec8a8
Michael Trani <mftc64@umsystem.edu>
2022-05-01 21:55:50 -0700

    Child process and shared memory successful.

--- oss/oss.cpp
+++ oss/oss.cpp
@@ -4,33 +4,121 @@
 
 #include "p6.h"
 
+void shared_memory_maker();
+void free_memory();
+
+
 static std::string logfile = "logfile";
 static FILE* fptr;
 
+struct Shmem* shmem;
+key_t skey, mkey;
+int sid;
+int mqid;
+
+
+
+std::string error_msg; 
+std::string exe_name; 
+
 int main(int argc, char* argv[]) {
 
-    // FOR ERROR HANDLING
-    std::string error_msg; // Errors
-    std::string exe_name; // Name of this executable
-
-    // Gets the executable name and removes "./"
     exe_name = argv[0];
-
+    error_msg += exe_name;
     fptr = fopen(logfile.c_str(), "w+");
 
-    int opt;
-    while ((opt = getopt(argc, argv, "m:h")) != -1) {
-        switch (opt) {
+    int option;
+    while ((option = getopt(argc, argv, "m:h")) != -1) {
+        switch (option) {
         case 'm':
             break;
         case 'h':
-            std::cout << "Your program should take in a command line option -m x, which accepts either a 0 or a 1, which determines how child processes will perform their memory access\n";
+            std::cout << "Your program should take in a command line optionion -m x, which accepts either a 0 or a 1, which determines how child processes will perform their memory access\n";
             return 0;
         default:
             break;
         }
     }
 
+    shared_memory_maker();
+
+    pid_t childpid;
+
+    switch (childpid = fork()) {
+    case -1:
+        error_msg += "Failed to fork";
+        perror(error_msg.c_str());
+        exit(EXIT_FAILURE);
+
+    case 0:
+        execl("./user", "user",  '\0');
+        error_msg = exe_name + ": Error: failed to execl";
+        perror(error_msg.c_str());
+        exit(EXIT_FAILURE);
+    }
+
+
+
+    sleep(1);
+
+
+    if (fptr)
+        fclose(fptr);
+    fptr = NULL;
+    free_memory();
 
     return 0;
+}
+
+
+// builds shared memory
+void shared_memory_maker() {
+
+    // ALLOCATE OR ATTACH TO SHARED MEMORY
+    if ((skey = ftok("makefile", 'a')) == (key_t)-1) {
+        error_msg += ":Shared Memory/skey/ftok - creating::";
+        perror(error_msg.c_str());
+        exit(EXIT_FAILURE);
+    }
+    if ((sid = shmget(skey, sizeof(struct Shmem), IPC_CREAT | 0666)) == -1) {
+        error_msg += ":Shared Memory/sid/shmget - creating::";
+        perror(error_msg.c_str());
+        exit(EXIT_FAILURE);
+    }
+
+    shmem = (struct Shmem*)shmat(sid, NULL, 0);
+
+
+    // SET UP MESSAGE QUEUE
+    if ((mkey = ftok("makefile", 'b')) == (key_t)-1) {
+        error_msg += ":Message Queue/mkey/ftok - creating";
+        perror(error_msg.c_str());
+        free_memory();
+        exit(EXIT_FAILURE);
+    }
+    // Create the queue
+    if ((mqid = msgget(mkey, 0644 | IPC_CREAT)) == -1) {
+        error_msg += ":Creating queue/mqid/msgget - ";
+        perror(error_msg.c_str());
+        free_memory();
+        exit(EXIT_FAILURE);
+    }
+
+
+}
+
+
+void free_memory() {
+
+    // Performs control operations on the message queue
+    if (msgctl(mqid, IPC_RMID, NULL) == -1) { // Removes the message queue
+        error_msg += " Could not remove the  message queue";
+        perror(error_msg.c_str());
+        exit(EXIT_FAILURE);
+    }
+
+    shmdt(shmem); 
+    shmctl(sid, IPC_RMID, NULL);
+    
+    printf("[OSS]: Memory cleared.\n");
 }
--- oss/user.cpp
+++ oss/user.cpp
@@ -1,7 +1,65 @@
 #include "p6.h"
 
-int main()
-{
+void shared_memory_maker();
+void sig_handler(int signal);
+
+
+struct Shmem* shmem;
+key_t skey, mkey;
+int sid, mqid;
+
+
+std::string error_msg;
+std::string exe_name;
+
+int main(int argc, char* argv[]) {
+    signal(SIGTERM, sig_handler); 
+    exe_name = argv[0];
+    error_msg += exe_name;
+
+    void shared_memory_maker();
+
+
     printf("hello from %s!\n", "user");
+
+
+    shmdt(shmem);
+
     return 0;
+}
+
+
+void shared_memory_maker() {
+    if ((skey = ftok("makefile", 'a')) == (key_t)-1) {
+        error_msg += ":skey/ftok - creating::";
+        perror(error_msg.c_str());
+        exit(EXIT_FAILURE);
+    }
+
+    if ((sid = shmget(skey, sizeof(struct Shmem), 0)) == -1) {
+        error_msg += "sid/shmget - allocation failure::";
+        perror(error_msg.c_str());
+        exit(EXIT_FAILURE);
+    }
+
+    shmem = (struct Shmem*)shmat(sid, NULL, 0);
+
+    if ((mkey = ftok("makefile", 'b')) == (key_t)-1) {
+        error_msg = ":mkey/ftok - creating msg queue";
+        perror(error_msg.c_str());
+        exit(EXIT_FAILURE);
+    }
+
+    if ((mqid = msgget(mkey, 0644)) == -1) {
+        error_msg = "mqid/msgget - allocate msg que failure::";
+        perror(error_msg.c_str());
+        exit(EXIT_FAILURE);
+    }
+}
+
+
+void sig_handler(int signal) { 
+    if (signal == 15) { // ctrl c;
+        exit(EXIT_FAILURE);
+    }
 }
